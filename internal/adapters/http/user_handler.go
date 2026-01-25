a88a9ce8f41c4dfcbc7ec0d19f704f043d422858
enriqueFcoG <enrikefrancisco@gmail.com>
2026-01-24 22:57:02 -0800

    user handler created

---
+++ internal/adapters/http/user_handler.go
@@ -0,0 +1,40 @@
+package http
+
+import (
+	"enriqueFcoG/cypher/internal/usecase"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UserHandler struct {
+	Service *usecase.UserService
+}
+
+func (h *UserHandler) Create(c *gin.Context) {
+	var req struct {
+		ID    string `json:"id"`
+		Email string `json:"string"`
+	}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	err := h.Service.CreateUser(req.ID, req.Email)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.Status(http.StatusCreated)
+}
+
+func (h *UserHandler) Get(c *gin.Context) {
+	id := c.Param("id")
+	user, err := h.Service.GetUser(id)
+	if err != nil {
+		c.JSON(404, gin.H{"error": "not found"})
+		return
+	}
+	c.JSON(200, user)
+}
