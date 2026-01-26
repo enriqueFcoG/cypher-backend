57fb89ee97dc516daa6a1118f25eab18ab331145
enriqueFcoG <enrikefrancisco@gmail.com>
2026-01-26 11:02:22 -0800

    update function in user repository removed

--- internal/ports/user_repository.go
+++ internal/ports/user_repository.go
@@ -5,5 +5,5 @@ import "enriqueFcoG/cypher/internal/domain"
 type UserRepository interface {
 	Save(u *domain.User) error
 	FindByID(id string) (*domain.User, error)
-	Update(u *domain.User) (*domain.User, error)
+	//Update(u *domain.User) (*domain.User, error)
 }
